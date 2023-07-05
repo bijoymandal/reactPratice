fd4be05ce89ded977660a49447bd966ab993a44e
bijoymandal <30389880+bijoymandal@users.noreply.github.com>
2023-07-05 21:35:15 +0530

    name tage using form

--- src/App.js
+++ src/App.js
@@ -3,31 +3,71 @@ import { useState } from 'react';
 import './App.css';
 
 const App = () =>{
-  const[name,setName]=useState("");
-  const[lasN,seLastName]=useState("");
-  const[fullName,setFullName]=useState();
-  const[LastName,setLastFullName]=useState();
+  // const[name,setName]=useState("");
+  // const[lasN,seLastName]=useState("");
+  const[fullName,setFullName]=useState({
+    fname:"",
+    lname:"",
+  });
+  // const[LastName,setLastFullName]=useState();
 
   const inputEvent=(event)=>{
     console.log(event.target.value);
-    setName(event.target.value);
+    console.log(event.target.name);
+    console.log(event.target.email);
+
+    // const value = event.target.value;
+    // const name = event.target.name;
+    // const email = event.target.name;
+    const {value,name} = event.target;
+
+    setFullName((preValue)=>{
+      //console.log(preValue);
+      if(name === 'fname')
+      {
+        return{
+          fname:value,
+          lname:preValue.lname,
+          email:preValue.email,
+        };
+      }
+      else if(name === 'lname')
+      {
+        return{
+          fname:preValue.fname,
+          lname:value,
+          email:preValue.email,
+        };
+      }
+      else if(name === 'email')
+      {
+        return{
+          fname:preValue.fname,
+          lname:preValue.lname,
+          email:value,
+        };
+      }
+    })
+    // setName(event.target.value);
   }
-  const inputLastEvent=(event)=>{
-    console.log(event.target.value);
-    seLastName(event.target.value);
-  }
+  // const inputLastEvent=(event)=>{
+  //   console.log(event.target.value);
+  //   seLastName(event.target.value);
+  // }
   const onSubmit=(event)=>{
     event.preventDefault();
-    setFullName(name);
-    setLastFullName(lasN);
+    // setFullName(name);
+    //setLastFullName(lasN);
   }
   return (
     <>
       <form onSubmit={onSubmit}>
-        <h1>Hello {fullName} {LastName}</h1>
-        <input type="text" placeholder='Enter FirstName' onChange={inputEvent} value={name}/>
+        <h1>Hello {fullName.fname} {fullName.lname} {fullName.email}</h1>
+        <input type="text" placeholder='Enter FirstName' name="fname" onChange={inputEvent} />
         <br />
-        <input type="text" placeholder='Enter LastName' onChange={inputLastEvent} value={lasN}/>
+        <input type="text" placeholder='Enter LastName' name="lname" onChange={inputEvent}/>
+        <br />
+        <input type="email" placeholder='Enter Email' name="email" onChange={inputEvent}/>
         <br />
         <button type='submit'>Submit</button>
       </form>
